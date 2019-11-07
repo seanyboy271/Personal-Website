2f686ee391805b496ad11de24bd3e795afb4e7bd
seanyboy271 <sean.kotrola@gmail.com>
2019-11-07 10:11:29 -0500

    Added fade-in animation

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,16 +5,18 @@ import './Home.css'
 import { Link } from 'react-router-dom'
 
 
+
 class Home extends Component {
+
     render() {
         return (
             <div className='homePage'>
                 <div className='section section1'>
-                    <div className='main-text'>
+                    <div className='main-text fade-in-main'>
                         Developer
                         <br />
                         <Link to='contact'>
-                            <button className='contactButton'>
+                            <button className='contactButton fade-in-button'>
                                 <div className='contactButtonText'>
                                     Contact me
                                 </div>
@@ -24,16 +26,17 @@ class Home extends Component {
                 </div>
 
 
-                <div className='section section2'>
-                    <div className='main-text-section2'>
-                        Who I am
+                <div className='bottom-section'>
+                    <div className='section2'>
+                        <div className='main-text-section2'>
+                            Who I am
+                        </div>
+
+                        <div className='sub-text'>
+                            {Util.Bio.createBio()}
+                        </div>
+
                     </div>
-
-                    <div className='sub-text'>
-                        {Util.Bio.createBio()}
-                    </div>
-
-
                 </div>
             </div>
         );
